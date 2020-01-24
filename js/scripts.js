99205b7e3e97499b00f631a0e128adf9e7edabec
Keturah Howard <keturah.dev@gmail.com>
2020-01-24 10:21:26 -0800

    achieve third spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,24 @@
 // B U S I N E S S   L O G I C
 var boopThis = function (number) {
-  var number = parseInt(number);
-  if (isNaN(number)) {
-    return 'please enter a number';
+
+  if (number.includes(' ', ',')) {
+    return 'please enter only one number'
   } else {
-    return number
+    var number = parseInt(number);
+    if (isNaN(number)) {
+      return 'please enter a number';
+    } else {
+      // var numberArray = number.split(" ")
+      // if (numberArray.length > 1) {
+      //   return 'please enter only one number';
+      // } else {
+      //   return number;
+      // }
+      return number;
+    };
+
   }
+
 };
 
 // U S E R   I N T E R F A C E   L O G I C
